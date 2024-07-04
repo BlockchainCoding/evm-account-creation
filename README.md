322a08ad24c2d77af9fdc2cfb3f84d71c982f48d
Patrick Malloy <6231534+PatrickMalloy@users.noreply.github.com>
2024-07-04 13:00:10 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Ethereum EVM Account Creation
+This code shows how to interact with an EVM compatible blockchain using Node.js to programmatically create accounts.
+
+The Ethereum Sepolia Testnet is used for this demostration.
+
+## Prerequisites
+- Node.js 20.7 or higher
+- Ethers.js ^6.6.5
+
+## Installation
+Clone the repository:
+```
+git clone https://github.com/BlockchainCoding/evm-account-creation.git
+```
+
+Change into new directory and install dependencies:
+```
+cd evm-account-creation;
+npm install
+```
+
+## Usage
+Enter the number of accounts you would like to create in the createAccount() function call at the end of the script.
+
+```
+npm run createAccount
+```
+or
+```
+node createAccount.js
+```
---
+++ createAccount.js
@@ -0,0 +1,52 @@
+import { Wallet, getDefaultProvider } from "ethers";
+import fs from "fs";
+
+// Use Ethers DefaultProvider for the Ethereum Sepolia Testnet
+const provider = getDefaultProvider("sepolia");
+
+function createAccount(_numAccounts) {
+  for (let count=1; count <= _numAccounts; count++ ) {
+    // Create a new random Ethereum account.
+    const walletRandom = Wallet.createRandom();
+    console.log(`[ACCOUNT_${count}]`);
+    saveAccount(walletRandom, `[ACCOUNT_${count}]`);
+    logAccount(walletRandom, "From Random");
+
+    // Create a new wallet using the Mnemonic from the first wallet.
+    // This simulates doing a wallet restore function
+    const walletPhrase = Wallet.fromPhrase(walletRandom.mnemonic.phrase);
+    logAccount(walletPhrase, "From Mnemonic");
+
+    // Create a new wallet using the Private Key from the first wallet.
+    // This can also be used to restore a wallet. Notice though you don't
+    // get a Mnemonic Phrase when creating from a Private Key
+    const walletPrivate = new Wallet(walletRandom.privateKey);
+    logAccount(walletPrivate, "From Private");
+  }
+}
+
+function saveAccount(_wallet, _header) {
+  const content = _header + "\n" +
+  "ADDRESS='" + _wallet.address + "'\n" +
+  "  PRIVATE_KEY='" + _wallet.privateKey + "'\n" +
+  "  MNEMONIC='" + _wallet.mnemonic.phrase + "'\n\n";
+
+  fs.writeFile('Accounts.txt', content, { flag: 'a'}, err => {
+    if(err) {
+      console.log(err);
+    }
+  });
+}
+
+function logAccount(wallet, msg) {
+console.log(`*** ${msg} ***`);
+console.log(`Address: ${wallet.address}`);
+console.log(`  Private Key: ${wallet.privateKey}`);
+if( wallet.mnemonic ) {
+  console.log(`  Mnemonic: ${wallet.mnemonic.phrase}`);
+}
+console.log();
+}
+
+// Create 3 new random accounts
+createAccount(3);
